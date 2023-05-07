ec42daa9179cca4e3997bc4fb11c75d55693cd0c
Bidhanhansda <bidhanhansda1997@gmail.com>
2023-05-08 02:06:30 +0530

    Ready to deployment

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const server = require("http").createServer(app);
 const PORT = 5001;
 const io = require("socket.io")(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: `${process.env.BASE_URL}`,
         methods: ["GET", "POST"]
     }
 })
